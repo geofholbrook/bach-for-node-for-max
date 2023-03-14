7ddcaf7ed27604899c2f92ede677f2334df45537
geofholbrook <geof.holbrook@gmail.com>
2023-03-14 18:13:42 -0400

    screwed up the formatting

--- main.ts
+++ main.ts
@@ -2,4 +2,4 @@ import { outlet } from 'max-api';
 import { generateBachAddChord } from './src/llll';
 
 outlet('clear');
-outlet(...generateBachAddChord([6000, 6400, 6700, 7100]).split(' '));
+outlet(...generateBachAddChord(         [6000, 6400, 6700, 7100]).split(' '));
